f1b99773340b17d4a48e40517760ac73be9f3d37
yulicahyani <yulicahyani1101@gmail.com>
2022-02-13 18:13:19 +0800

    membuat tampilan dashboard

--- app/src/main/java/com/yulicahyani/eraport/ui/login/LoginActivity.kt
+++ app/src/main/java/com/yulicahyani/eraport/ui/login/LoginActivity.kt
@@ -1,12 +1,25 @@
 package com.yulicahyani.eraport.ui.login
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.yulicahyani.eraport.R
+import com.yulicahyani.eraport.databinding.ActivityLoginBinding
+import com.yulicahyani.eraport.ui.dashboard.DashboardActivity
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var activityLoginBinding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        activityLoginBinding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(activityLoginBinding.root)
+
+        activityLoginBinding.btnLogin.setOnClickListener {
+            val intent = Intent(this@LoginActivity, DashboardActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+        }
     }
 }
