8fc158f8dafd8fece0fccf49ca1ede197e4a360b
yulicahyani <yulicahyani1101@gmail.com>
2022-03-13 23:34:38 +0800

    membuat tampilan tambah data user

--- app/src/main/java/com/yulicahyani/eraport/ui/datautama/datauser/DataUserActivity.kt
+++ app/src/main/java/com/yulicahyani/eraport/ui/datautama/datauser/DataUserActivity.kt
@@ -1,12 +1,65 @@
 package com.yulicahyani.eraport.ui.datautama.datauser
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.yulicahyani.eraport.R
+import com.yulicahyani.eraport.databinding.ActivityDataSiswaBinding
+import com.yulicahyani.eraport.databinding.ActivityDataUserBinding
+import com.yulicahyani.eraport.ui.dashboard.DashboardActivity
+import com.yulicahyani.eraport.ui.datautama.DataUtamaActivity
+import com.yulicahyani.eraport.ui.datautama.datasiswa.TambahDataSiswaActivity
+import com.yulicahyani.eraport.ui.inputnilai.InputNilaiActivity
+import com.yulicahyani.eraport.ui.raport.RaportActivity
 
 class DataUserActivity : AppCompatActivity() {
+
+    private lateinit var activityDataUserBinding: ActivityDataUserBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_data_user)
+        activityDataUserBinding = ActivityDataUserBinding.inflate(layoutInflater)
+        setContentView(activityDataUserBinding.root)
+
+        val item = activityDataUserBinding.navigation.menu.findItem(R.id.nav_data_utama)
+        item.isChecked = true
+        navigationListener()
+
+        activityDataUserBinding.btnTambahUser.setOnClickListener {
+            val intent = Intent(this@DataUserActivity, TambahDataUserActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+    private fun navigationListener() {
+        activityDataUserBinding.navigation.setOnItemSelectedListener { item ->
+            when (item.itemId) {
+                R.id.nav_dashboard -> {
+                    val intent = Intent(this@DataUserActivity, DashboardActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    startActivity(intent)
+                    return@setOnItemSelectedListener true
+                }
+                R.id.nav_data_utama -> {
+                    val intent = Intent(this@DataUserActivity, DataUtamaActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    startActivity(intent)
+                    return@setOnItemSelectedListener true
+                }
+                R.id.nav_input_nilai -> {
+                    val intent = Intent(this@DataUserActivity, InputNilaiActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    startActivity(intent)
+                    return@setOnItemSelectedListener true
+                }
+                R.id.nav_raport -> {
+                    val intent = Intent(this@DataUserActivity, RaportActivity::class.java)
+                    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+                    startActivity(intent)
+                    return@setOnItemSelectedListener true
+                }
+            }
+            false
+        }
     }
 }
