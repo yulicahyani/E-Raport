5fe9d10ba12254ca341f912811c5ecd44c00e2df
arisurya <arisurya425@gmail.com>
2022-03-17 02:08:37 +0800

    Added ui nilai spiritual and sosial

--- app/src/main/java/com/yulicahyani/eraport/ui/inputnilai/nilaisikapspiritual/NilaiSpiritualActivity.kt
+++ app/src/main/java/com/yulicahyani/eraport/ui/inputnilai/nilaisikapspiritual/NilaiSpiritualActivity.kt
@@ -3,6 +3,10 @@ package com.yulicahyani.eraport.ui.inputnilai.nilaisikapspiritual
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import com.yulicahyani.eraport.R
 import com.yulicahyani.eraport.databinding.ActivityNilaiSpiritualBinding
 import com.yulicahyani.eraport.ui.dashboard.DashboardActivity
@@ -20,7 +24,7 @@ class NilaiSpiritualActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         activityNilaiSpiritualBinding = ActivityNilaiSpiritualBinding.inflate(layoutInflater)
         setContentView(activityNilaiSpiritualBinding.root)
-
+        setupSpinner()
         val item = activityNilaiSpiritualBinding.navigation.menu.findItem(R.id.nav_input_nilai)
         item.isChecked = true
         navigationListener()
@@ -57,4 +61,123 @@ class NilaiSpiritualActivity : AppCompatActivity() {
             false
         }
     }
+
+    private fun setupSpinner() {
+        val arrayNilaiSikap = arrayOf("Pilih nilai", "SB", "B", "PB")
+        val arrayAdapter =
+            ArrayAdapter(this, android.R.layout.simple_spinner_item, arrayNilaiSikap)
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        //Ketaatan Beribadah
+        val spinnerBeribadah = activityNilaiSpiritualBinding.spinnerBeribadah
+        spinnerBeribadah.adapter = arrayAdapter
+        val nilaiSikapBeribadah = "Pilih nilai"
+        arrayNilaiSikap.forEachIndexed { index, nilai ->
+            if (nilaiSikapBeribadah == nilai) {
+                spinnerBeribadah.setSelection(index)
+            }
+        }
+        spinnerBeribadah.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View,
+                position: Int,
+                id: Long
+            ) {
+                val penilaian = arrayNilaiSikap[position]
+                Toast.makeText(
+                    this@NilaiSpiritualActivity,
+                    " $penilaian ",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            override fun onNothingSelected(parent: AdapterView<*>) {
+            }
+        }
+
+
+        //Berprilaku Bersyukur
+        val spinnerBersyukur = activityNilaiSpiritualBinding.spinnerBersyukur
+        spinnerBersyukur.adapter = arrayAdapter
+        val nilaiSikapBersyukur = "Pilih nilai"
+        arrayNilaiSikap.forEachIndexed { index, nilai ->
+            if (nilaiSikapBersyukur == nilai) {
+                spinnerBersyukur.setSelection(index)
+            }
+        }
+        spinnerBersyukur.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View,
+                position: Int,
+                id: Long
+            ) {
+                val penilaian = arrayNilaiSikap[position]
+                Toast.makeText(
+                    this@NilaiSpiritualActivity,
+                    " $penilaian ",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            override fun onNothingSelected(parent: AdapterView<*>) {
+            }
+        }
+
+        //Berdoa
+        val spinnerBerdoa = activityNilaiSpiritualBinding.spinnerBerdoa
+        spinnerBerdoa.adapter = arrayAdapter
+        val nilaiSikapBerdoa = "Pilih nilai"
+        arrayNilaiSikap.forEachIndexed { index, nilai ->
+            if (nilaiSikapBerdoa == nilai) {
+                spinnerBerdoa.setSelection(index)
+            }
+        }
+        spinnerBerdoa.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View,
+                position: Int,
+                id: Long
+            ) {
+                val penilaian = arrayNilaiSikap[position]
+                Toast.makeText(
+                    this@NilaiSpiritualActivity,
+                    " $penilaian ",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            override fun onNothingSelected(parent: AdapterView<*>) {
+            }
+        }
+
+        //Toleransi
+        val spinnerToleransi = activityNilaiSpiritualBinding.spinnerToleransi
+        spinnerToleransi.adapter = arrayAdapter
+        val nilaiSikapToleransi = "Pilih nilai"
+        arrayNilaiSikap.forEachIndexed { index, nilai ->
+            if (nilaiSikapToleransi == nilai) {
+                spinnerToleransi.setSelection(index)
+            }
+        }
+        spinnerToleransi.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(
+                parent: AdapterView<*>,
+                view: View,
+                position: Int,
+                id: Long
+            ) {
+                val penilaian = arrayNilaiSikap[position]
+                Toast.makeText(
+                    this@NilaiSpiritualActivity,
+                    " $penilaian ",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+            override fun onNothingSelected(parent: AdapterView<*>) {
+            }
+        }
+
+
+    }
+
 }
