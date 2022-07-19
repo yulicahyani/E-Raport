ea3e533c717373b0bd31b586211eb0d7eb7dbbf9
arisurya7 <arisurya425@gmail.com>
2022-07-19 16:55:36 +0800

    add count in dashboard

--- app/src/main/java/com/yulicahyani/eraport/ui/dashboard/DashboardActivity.kt
+++ app/src/main/java/com/yulicahyani/eraport/ui/dashboard/DashboardActivity.kt
@@ -3,7 +3,12 @@ package com.yulicahyani.eraport.ui.dashboard
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.yulicahyani.eraport.R
+import com.yulicahyani.eraport.data.source.remote.api.ApiConfig
+import com.yulicahyani.eraport.data.source.remote.response.MapelResponse
+import com.yulicahyani.eraport.data.source.remote.response.SiswaResponse
+import com.yulicahyani.eraport.data.source.remote.response.UserResponse
 import com.yulicahyani.eraport.databinding.ActivityDashboardBinding
 import com.yulicahyani.eraport.helper.Constant
 import com.yulicahyani.eraport.helper.PrefHelper
@@ -11,6 +16,9 @@ import com.yulicahyani.eraport.ui.datautama.DataUtamaActivity
 import com.yulicahyani.eraport.ui.inputnilai.InputNilaiActivity
 import com.yulicahyani.eraport.ui.profil.ProfilActivity
 import com.yulicahyani.eraport.ui.raport.RaportActivity
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class DashboardActivity : AppCompatActivity() {
 
@@ -31,6 +39,48 @@ class DashboardActivity : AppCompatActivity() {
         val fullName = StringBuilder()
         activityDashboardBinding.tvNama.text = fullName.append("Hai, ").append(prefHelper.getString(Constant.PREF_FIRSTNAME)).append("!")
 
+        //Count Dashboard
+        ApiConfig.getApiService().getMapel().enqueue(object : Callback<MapelResponse> {
+            override fun onResponse(call: Call<MapelResponse>, response: Response<MapelResponse>) {
+                if (response.isSuccessful){
+                    if (response.body()?.status == 1){
+                        val countStringMapel = StringBuilder()
+                        activityDashboardBinding.countMapel.text = countStringMapel.append(response.body()?.mapels?.size.toString()).append(" ").append("Mapel")
+                    }
+                }
+            }
+            override fun onFailure(call: Call<MapelResponse>, t: Throwable) {
+                Log.e("", "onFailure: ${t.message.toString()}")
+            }
+        })
+
+        ApiConfig.getApiService().getAllUser().enqueue(object :Callback<UserResponse> {
+            override fun onResponse(call: Call<UserResponse>, response: Response<UserResponse>) {
+                if (response.isSuccessful){
+                    if (response.body()?.status == 1){
+                        val countStringUser = StringBuilder()
+                        activityDashboardBinding.countUser.text = countStringUser.append(response.body()?.user?.size.toString()).append(" ").append("User")
+                    }
+                }
+            }
+            override fun onFailure(call: Call<UserResponse>, t: Throwable) {
+                Log.e("", "onFailure: ${t.message.toString()}")
+            }
+        })
+
+        ApiConfig.getApiService().getSiswaSekolah(prefHelper.getInt(Constant.PREF_ID_USER)).enqueue(object :Callback<SiswaResponse>{
+            override fun onResponse(call: Call<SiswaResponse>, response: Response<SiswaResponse>) {
+                if (response.isSuccessful){
+                    if (response.body()?.status == 1){
+                        val countStringSiswa = StringBuilder()
+                        activityDashboardBinding.countSiswa.text = countStringSiswa.append(response.body()?.siswa?.size.toString()).append(" ").append("Siswa")
+                    }
+                }
+            }
+            override fun onFailure(call: Call<SiswaResponse>, t: Throwable) {
+                Log.e("", "onFailure: ${t.message.toString()}")
+            }
+        })
 
         activityDashboardBinding.user.setOnClickListener {
             val intent = Intent(this@DashboardActivity, ProfilActivity::class.java)
